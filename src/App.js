1de5b7b3028ef89d964f18641e4729364a530c04
shakizWasTaken <yahya2003rhemmali@gmail.com>
2022-04-29 13:16:37 +0000

    assignment done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './styles.css';
+import imageInSrc from './imageInSrc.jpeg'
 
 function App() {
+
+  const myName = "Yahya Rhemmali";
+
+  const video = {
+    src : "/myVideo.mp4",
+    width:"320px",
+    height:"240px",
+    controls : true,
+    type:"video/mp4" 
+  }
+
   return (
     <div className="App">
-      
+      <div>
+        <h1>{myName}</h1>
+        <br />
+        <img src={imageInSrc} alt="" />
+        <br />
+        <img src="imageInPublic.jpeg" alt="" />
+      </div> 
+      <video {...video}></video>
     </div>
   );
 }
